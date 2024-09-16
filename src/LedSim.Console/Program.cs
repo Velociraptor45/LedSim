3dcf0ebebdb11d34fc4abb01839b2dd3170dd4ba
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-09-16 20:18:22 +0200

    Add base Led class

---
+++ src/LedSim.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using LedSim;
+using Spectre.Console;
+
+const int rowCount = 3;
+const int columnCount = 5;
+
+var led = new Led(columnCount, rowCount, Color.Black);
+
+var random = new Random(28);
+
+for (int row = 0; row < rowCount; row++)
+{
+    for (int col = 0; col < columnCount; col++)
+    {
+        led.Wait(500);
+        led.Clear(false);
+        led[col, row] = new Color(
+            (byte)random.Next(255),
+            (byte)random.Next(255),
+            (byte)random.Next(255)
+            );
+    }
+}
+
+led.Wait(500);
+led.Clear();
---
+++ src/LedSim/Led.cs
@@ -0,0 +1,78 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace LedSim;
+
+public class Led
+{
+    private readonly Color[,] _colors;
+    private readonly Color _defaultColor;
+
+    public Led(int columnCount, int rowCount) : this(columnCount, rowCount, Color.Black)
+    {
+    }
+
+    public Led(int columnCount, int rowCount, Color defaultColor)
+    {
+        _colors = new Color[columnCount, rowCount];
+        _defaultColor = defaultColor;
+        Clear();
+        Render();
+    }
+
+    public Color this[int column, int row]
+    {
+        get => _colors[column, row];
+        set
+        {
+            _colors[column, row] = value;
+            Render();
+        }
+    }
+
+    public void Wait(int milliseconds)
+    {
+        Task.Delay(milliseconds).GetAwaiter().GetResult();
+    }
+
+    public void Clear(bool renderOnClear = true)
+    {
+        for (int row = 0; row < _colors.GetLength(1); row++)
+        {
+            for (byte col = 0; col < _colors.GetLength(0); col++)
+            {
+                _colors[col, row] = _defaultColor;
+            }
+        }
+
+        if (renderOnClear)
+            Render();
+    }
+
+    private void Render()
+    {
+        var grid = new Grid();
+
+        for (byte col = 0; col < _colors.GetLength(0); col++)
+        {
+            grid.AddColumn(new GridColumn()
+            {
+                Padding = new Padding(0, 0, 2, 0),
+            });
+        }
+
+        for (byte row = 0; row < _colors.GetLength(1); row++)
+        {
+            var cells = new List<IRenderable>();
+            for (byte col = 0; col < _colors.GetLength(0); col++)
+            {
+                cells.Add(new Text("  ", new Style(null, _colors[col, row])));
+            }
+            grid.AddRow(cells.ToArray());
+            grid.AddEmptyRow();
+        }
+
+        AnsiConsole.Clear();
+        AnsiConsole.Write(grid);
+    }
+}
